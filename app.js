7199d176d537fd4a51aa14263d189e47e65e40fd
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-03-03 17:44:12 +0100

    Added new functions and updated UI
    Added new buttons to change grid siza and reset grid
    Added functions to change grid size and color on mouseover

--- app.js
+++ app.js
@@ -1,9 +1,57 @@
 const gridContainer = document.querySelector("#container");
+let gridSize = 20;
+let pixel = 15;
+let containerSize = gridSize * pixel;
 
-for (let i = 1; i <= 400; i++) {
-  const div = document.createElement("div");
-  div.style.cssText =
-    "height: 24px; width: 24px; border: solid 0.5px black; margin: 0; padding: 0";
-  // div.textContent = i;
-  gridContainer.appendChild(div);
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", createGrid);
+
+const setGrid = document.querySelector("#set-grid");
+setGrid.addEventListener("click", () => {
+  gridSize = prompt("Grid size: ", 20);
+  containerSize = gridSize * pixel;
+  createGrid();
+});
+
+function deleteGrids() {
+  allGrid = document.querySelectorAll(".grid");
+  allGrid.forEach((grid) => {
+    gridContainer.removeChild(grid);
+  });
 }
+
+function randomNumber(num) {
+  return Math.floor(Math.random() * (num + 1));
+}
+
+function createGrid() {
+  deleteGrids();
+  gridContainer.style.cssText = `width: ${containerSize}px; height: ${containerSize}px`;
+  for (let i = 1; i <= gridSize * gridSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    gridContainer.appendChild(div);
+  }
+  colorGridRandom();
+}
+
+function colorGrid() {
+  const grids = document.querySelectorAll(".grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "black";
+    });
+  });
+}
+function colorGridRandom() {
+  const grids = document.querySelectorAll(".grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = `rgb(${randomNumber(256)}, ${randomNumber(
+        256
+      )}, ${randomNumber(256)})`;
+    });
+  });
+}
+
+createGrid();
